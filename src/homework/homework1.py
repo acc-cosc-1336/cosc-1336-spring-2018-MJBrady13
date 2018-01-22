d003fa356c97497c520ae3dab01a45933ace2f10
MJBrady13 <35587138+MJBrady13@users.noreply.github.com>
2018-01-22 10:01:11 -0600

    Update homework1.py
    Received the same error in the log on TravisCi after applying the suggestions from the Blackboard discussion thread. I've made some adjustments to test again. Each submission works locally, however I haven't been able to get the TravisCi to accept it yet. -Michael Brady

--- src/homework/homework1.py
+++ src/homework/homework1.py
@@ -2,24 +2,24 @@
 
 #Finds the number of hours
 def get_hours_since_midnight(total_seconds):
-  hours = total_seconds // 3600
-  return hours
+    hours = int(total_seconds // 3600)
+    return hours
 
 #Finds the number of minutes
-def get_minutes(total_seconds):
-  seconds_remaining = total_seconds % 3600
-  minutes = seconds_remaining // 60
-  return minutes
+def get_minutes(total_seconds): 
+    seconds_remaining = int(total_seconds % 3600)
+    minutes = int(seconds_remaining // 60)
+    return minutes
 
 #Finds the number of seconds
-def get_seconds(total_seconds):
-  seconds_remaining = total_seconds % 3600
-  seconds = seconds_remaining % 60
-  return seconds
+def get_seconds(total_seconds): 
+    seconds_remaining = int(total_seconds % 3600)
+    seconds = int(seconds_remaining % 60)
+    return seconds
 
-total_seconds = int(input('Enter number of seconds: '))    
-final_hours = get_hours_since_midnight(total_seconds)    
-final_minutes = get_minutes(total_seconds)    
-final_seconds = get_seconds(total_seconds)    
-print ('The formatted time is: ', final_hours, ':', final_minutes,           
-         ':', final_seconds)
+total_seconds = int(input('Enter number of seconds: '))
+final_hours = int(get_hours_since_midnight(total_seconds))
+final_minutes = int(get_minutes(total_seconds))
+final_seconds = int(get_seconds(total_seconds))
+print ('The formatted time is: ', final_hours, ':', final_minutes,
+           ':', final_seconds)

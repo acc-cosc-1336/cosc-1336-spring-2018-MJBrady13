71025913591c1b54101da72f26c49b796989a0c7
MJBrady13 <35587138+MJBrady13@users.noreply.github.com>
2018-01-22 01:45:46 +0000

    Update homework1.py
    This program now converts a given number of seconds into hours, minutes and seconds. The main() function requests the user enter the total number of seconds to convert. That number is passed to the three functions which finds the corresponding hours, minutes and remaining seconds and returns them to be printed to screen. The digital clock format isn't perfect and is something I will continue to work on. -Michael Brady

--- src/homework/homework1.py
+++ src/homework/homework1.py
@@ -1,6 +1,28 @@
 #This program converts the seconds past midnight into hours, minutes and seconds
-def main():    total_seconds = int(input('Enter number of seconds: '))    final_hours = get_hours_since_midnight(total_seconds)    final_minutes = get_minutes(total_seconds)    final_seconds = get_seconds(total_seconds)    print ('The formatted time is: ', final_hours, ':', final_minutes,           ':', final_seconds)
-#Finds the number of hoursdef get_hours_since_midnight(total_seconds):    hours = total_seconds // 3600    return hours
-#Finds the number of minutesdef get_minutes(total_seconds):     seconds_remaining = total_seconds % 3600    minutes = seconds_remaining // 60    return minutes
-#Finds the number of secondsdef get_seconds(total_seconds):     seconds_remaining = total_seconds % 3600    seconds = seconds_remaining % 60    return seconds
+
+def main():    
+  total_seconds = int(input('Enter number of seconds: '))    
+  final_hours = get_hours_since_midnight(total_seconds)    
+  final_minutes = get_minutes(total_seconds)    
+  final_seconds = get_seconds(total_seconds)    
+  print ('The formatted time is: ', final_hours, ':', final_minutes,           
+         ':', final_seconds)
+  
+#Finds the number of hours
+def get_hours_since_midnight(total_seconds):
+  hours = total_seconds // 3600
+  return hours
+
+#Finds the number of minutes
+def get_minutes(total_seconds):
+  seconds_remaining = total_seconds % 3600
+  minutes = seconds_remaining // 60
+  return minutes
+
+#Finds the number of seconds
+def get_seconds(total_seconds):
+  seconds_remaining = total_seconds % 3600
+  seconds = seconds_remaining % 60
+  return seconds
+
 main()

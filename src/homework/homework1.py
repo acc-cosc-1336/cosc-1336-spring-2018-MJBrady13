b43d958e8e85fb91ff988c715df3adc80dea5619
MJBrady13 <35587138+MJBrady13@users.noreply.github.com>
2018-01-22 16:36:15 -0600

    Update homework1.py
    Removed all code outside the three functions. My apologies. I thought we had to have a working program. -Michael Brady

--- src/homework/homework1.py
+++ src/homework/homework1.py
@@ -16,10 +16,3 @@ def get_seconds(total_seconds):
     seconds_remaining = int(total_seconds % 3600)
     seconds = int(seconds_remaining % 60)
     return seconds
-
-total_seconds = int(input('Enter number of seconds: '))
-final_hours = int(get_hours_since_midnight(total_seconds))
-final_minutes = int(get_minutes(total_seconds))
-final_seconds = int(get_seconds(total_seconds))
-print ('The formatted time is: ', final_hours, ':', final_minutes,
-           ':', final_seconds)

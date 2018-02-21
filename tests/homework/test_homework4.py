12ff456777e37f4d99f62bf0e483b6fe05ddc4bd
MJBrady13 <35587138+MJBrady13@users.noreply.github.com>
2018-02-21 10:46:14 -0600

    Update test_homework4.py

--- tests/homework/test_homework4.py
+++ tests/homework/test_homework4.py
@@ -19,49 +19,49 @@ class TestHomework2(unittest.TestCase):
         #self.assertEqual(1, sample_function(1));
 
     #WRITE 5 TESTS FOR get_credit_points with the letter A, B, C, D, and F as arguments
-        def test_get_credit_points_A(self):
-            self.assertEqual(4.0, get_credit_points('A'))
+    def test_get_credit_points_A(self):
+        self.assertEqual(4.0, get_credit_points('A'))
 
-        def test_get_credit_points_B(self):
-            self.assertEqual(3.0, get_credit_points('B'))
+    def test_get_credit_points_B(self):
+        self.assertEqual(3.0, get_credit_points('B'))
 
-        def test_get_credit_points_C(self):
-            self.assertEqual(2.0, get_credit_points('C'))
+    def test_get_credit_points_C(self):
+        self.assertEqual(2.0, get_credit_points('C'))
 
-        def test_get_credit_points_D(self):
-            self.assertEqual(1.0, get_credit_points('D'))
+    def test_get_credit_points_D(self):
+        self.assertEqual(1.0, get_credit_points('D'))
 
-        def test_get_credit_points_F(self):
-            self.assertEqual(0.0, get_credit_points('F'))
+    def test_get_credit_points_F(self):
+        self.assertEqual(0.0, get_credit_points('F'))
 
     #WRITE 5 TESTS FOR get_credit_points with the letter a, b, c, d, and f as arguments
-        def test_get_credit_points_a(self):
-            self.assertEqual(4.0, get_credit_points('a'))
+    def test_get_credit_points_a(self):
+        self.assertEqual(4.0, get_credit_points('a'))
 
-        def test_get_credit_points_b(self):
-            self.assertEqual(3.0, get_credit_points('b'))
+    def test_get_credit_points_b(self):
+        self.assertEqual(3.0, get_credit_points('b'))
 
-        def test_get_credit_points_c(self):
-            self.assertEqual(2.0, get_credit_points('c'))
+    def test_get_credit_points_c(self):
+        self.assertEqual(2.0, get_credit_points('c'))
 
-        def test_get_credit_points_d(self):
-            self.assertEqual(1.0, get_credit_points('d'))
+    def test_get_credit_points_d(self):
+        self.assertEqual(1.0, get_credit_points('d'))
 
-        def test_get_credit_points_f(self):
-            self.assertEqual(0.0, get_credit_points('f'))
+    def test_get_credit_points_f(self):
+        self.assertEqual(0.0, get_credit_points('f'))
 
     #WRITE A TEST FOR valid_letter_grade with letter B as argument
-        def test_valid_letter_grade_B(self):
-            self.assertEqual(True, valid_letter_grade('B'))
+    def test_valid_letter_grade_B(self):
+        self.assertEqual(True, valid_letter_grade('B'))
 
     #WRITE A TEST FOR valid_letter_grade with letter Z as argument
-        def test_valid_letter_grade_Z(self):
-            self.assertEqual(False, valid_letter_grade('Z'))
+    def test_valid_letter_grade_Z(self):
+        self.assertEqual(False, valid_letter_grade('Z'))
 
     #WRITE A TEST FOR get_grade_points with arguments 3 and 4
-        def test_get_grade_points_3_4(self):
-            self.assertEqual(12, get_grade_points(3,4))
+    def test_get_grade_points_3_4(self):
+        self.assertEqual(12, get_grade_points(3,4))
 
     #WRITE A TEST FOR get_grade_point_average with arguments 9.0 and 36.0
-        def test_get_grade_point_average_9_36(self):
-            self.assertEqual(4.0, get_grade_point_average(9.0, 36.0))
+    def test_get_grade_point_average_9_36(self):
+        self.assertEqual(4.0, get_grade_point_average(9.0, 36.0))

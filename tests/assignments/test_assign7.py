20aa06ac0cf27484e0e864d5ada51002bcea9989
MJBrady13 <35587138+MJBrady13@users.noreply.github.com>
2018-03-01 22:16:22 -0600

    Add files via upload

--- tests/assignments/test_assign7.py
+++ tests/assignments/test_assign7.py
@@ -1,6 +1,6 @@
 import unittest
 #write the import for function for assignment7 sum_list_values
-
+from src.assignments.assignment7 import sum_list_values
 
 class Test_Assign7(unittest.TestCase):
 
@@ -9,3 +9,6 @@ class Test_Assign7(unittest.TestCase):
 
     #create a test for the sum_list_values function with list elements:
     # bill 23 16 19 22
+    def test_sum_list_values_bill_23_16_19_22(self):
+        test_list = ['bill', 23, 16, 19, 22]
+        self.assertEqual(80, sum_list_values(test_list))

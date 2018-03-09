a63d02ad6270670feb406717f10b82851271883a
MJBrady13 <35587138+MJBrady13@users.noreply.github.com>
2018-03-08 20:14:04 -0600

    Add files via upload

---
+++ tests/Midterm/exam_test.py
@@ -0,0 +1,53 @@
+import unittest
+
+#write import statements for exam functions
+
+class Test_Midterm(unittest.TestCase):
+
+    def test_get_miles_per_hour(self):
+        '''
+        5 points
+        Test with arguments kilometers 32 and minutes 60 return value should be 19.883872
+        '''
+
+
+
+    def test_get_bonus_pay_amount_w_good_value (self):
+        '''
+        5 points
+        Test with value 1000 return value should be 70
+        '''
+
+
+
+    def test_get_bonus_pay_amount_w_bad_value(self):
+        '''
+        5 points
+        Test with -5 return value should be 'Invalid arguments'
+        '''
+
+
+
+    def test_reverse_string(self):
+        '''
+        5 points
+        Test with value My String Data return value should be ataD gnirtS yM
+        '''
+
+
+
+    def test_get_list_min_max(self):
+        '''
+        5 points
+        Test with ['joe', 10, 15, 20, 30, 40]    Returns:    [10, 40]
+        '''
+
+
+
+    def test_get_list_min_max_file(self):
+        '''
+        5 points
+        Test with quiz.data file the return value should be [2,89]
+        '''
+
+    

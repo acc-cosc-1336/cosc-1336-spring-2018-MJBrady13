ca246907db78344fe0276729731017f52d0ac8a0
MJBrady13 <35587138+MJBrady13@users.noreply.github.com>
2018-04-05 17:01:42 -0500

    Add files via upload

---
+++ tests/homework/test_homework9.py
@@ -0,0 +1,17 @@
+import unittest
+
+#Write the import statement for the Die class
+from src.homework.homework9.die import Die
+
+class TestHomework9(unittest.TestCase):
+
+   
+
+    def test_rolls_values_1_to_6(self):
+        '''
+        Write a test case to ensure that the Die class only rolls values from 1 to 6
+
+        '''
+        die = Die()
+        num = die.roll()
+        self.assertTrue(1 <= num <= 6)
